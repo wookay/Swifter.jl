7708d275a02c45a227b77459aa8f97a2642e980a
WooKyoung Noh <wookay.noh@gmail.com>
2016-06-23 20:21:39 -0400

    jupyter notebook mimetypes for array

--- src/jupyter.jl
+++ src/jupyter.jl
@@ -6,7 +6,9 @@ import Requests: get
 import URIParser: escape
 import JSON: print
 
+
 Base.mimewritable(::Type{MIME"text/markdown"}, result::QueryResult) = true
+
 
 function path_of_image_from_param(param::Dict)
     lhs = param["lhs"]
@@ -16,13 +18,25 @@ function path_of_image_from_param(param::Dict)
     (escape(sprint(print,lhs)), simple)
 end
 
-function image_scale(result::QueryResult)
-    m = match(r"= \(([\d\.]*) ([\d\.]*); ([\d\.]*) ([\d\.]*)\);", result.info.value)
+
+function look_like_view(str::AbstractString)
+    m = match(r"(0x[\dabcdef]*); frame = \(([\d\.]*) ([\d\.]*); ([\d\.]*) ([\d\.]*)\);", str)
     if !isa(m, RegexMatch)
-        m = match(r"= {{([\d\.]*), ([\d\.]*)}, {([\d\.]*), ([\d\.]*)}};", result.info.value)
+        m = match(r"(0x[\dabcdef]*); frame = {{([\d\.]*), ([\d\.]*)}, {([\d\.]*), ([\d\.]*)}};", str)
     end
     if isa(m, RegexMatch)
-        (x,y,w,h) = map(n->parse(Float32,n), m.captures)
+        (address, xywh) = (m.captures[1], m.captures[2:end])
+        (true, address, map(n->parse(Float32,n), xywh))
+    else
+        (false, nothing, nothing)
+    end
+end
+
+
+function image_scale(str::AbstractString)
+    (isview, address, xywh) = look_like_view(str)
+    if isview
+        (x,y,w,h) = xywh
         scale = 1
         if w > 1000 && h > 500
             scale = 1/5
@@ -35,6 +49,7 @@ function image_scale(result::QueryResult)
     end
 end
 
+
 function save_image_url_to_file(url::AbstractString, path::AbstractString, r::AbstractString)
     isempty(path) && return url
     isdir("images") || mkdir("images")
@@ -45,9 +60,53 @@ function save_image_url_to_file(url::AbstractString, path::AbstractString, r::Ab
     image_path
 end
 
+
+function escape_html(str::AbstractString)
+    s = replace(str, "&", "&amp;")
+    s = replace(s, ">", "&gt;")
+    replace(s, "<", "&lt;")
+end
+
+
+function save_image(url::AbstractString, alt::AbstractString)
+    randhash = randstring(10)
+    src = "$url&r=$randhash"
+    save_to_local = true
+    if haskey(ENV, "SWIFTER_SAVE_IMAGE")
+        save_to_local = !(ENV["SWIFTER_SAVE_IMAGE"] in [false, "false", "0"])
+    end
+    if save_to_local # && !isempty(el)
+        image_path = save_image_url_to_file(src, alt, randhash)
+        src = "$image_path?r=$randhash"
+    end
+    src
+end
+
+
+function show_view_vector(stream::IO, mime::MIME"text/markdown", appvec::Vector{App}, vec::Vector)
+    for (idx,el) in enumerate(vec)
+        isa(el, Void) && continue
+        (isview, address, xywh) = look_like_view(el)
+        if isview
+            write(stream, escape_html(el))
+            app = appvec[idx]
+            url = "$(app.url)/image?address=$address"
+            tag(stream, :img, :src=>save_image(url, address), :alt=>address, :style=>image_scale(el))
+        else
+            plain(stream, escape_html(el))
+        end
+    end
+end
+
+
+function Base.show(stream::IO, mime::MIME"text/markdown", vec::AbstractArray{QueryResult,1})
+    show_view_vector(stream, mime, map(v->v.app, vec), map(v->v.info.value, vec))
+end
+
+
 function Base.show(stream::IO, mime::MIME"text/markdown", result::QueryResult; kwargs...)
     if isa(result.info.value, AbstractArray)
-        plain(stream, List(map(htmlesc, result.info.value)))
+        show_view_vector(stream, mime, [result.app for x in 1:length(result.info.value)], result.info.value)
     elseif isa(result.info.value, Void)
         # nothing
     else
@@ -55,23 +114,19 @@ function Base.show(stream::IO, mime::MIME"text/markdown", result::QueryResult; k
     end
     if :view == result.info.typ
         (path,simple) = path_of_image_from_param(result.param)
-        r = randstring(10)
-        url = "$(result.app.url)/image?path=$path&r=$r"
-        save_image = true
-        if haskey(ENV, "SWIFTER_SAVE_IMAGE")
-            save_image = !(ENV["SWIFTER_SAVE_IMAGE"] in [false, "false", "0"])
-        end
-        if save_image && !isempty(result.app.url)
-            image_path = save_image_url_to_file(url, simple, r)
-            url = "$image_path?r=$r"
-        end
-        tag(stream, :img, :src=>url, :alt=>simple, :style=>image_scale(result))
+        app = result.app
+        url = "$(app.url)/image?path=$path"
+        tag(stream, :img, :src=>save_image(url, simple), :alt=>simple, :style=>image_scale(result.info.value))
     end
 end
 
 
 # #14052  ae62bf0b813afbf32402874451e55d16de909bd4
 if VERSION < v"0.5-dev+4341"
+    function Base.writemime(stream::IO, mime::MIME"text/markdown", vec::AbstractArray{QueryResult,1}; kwargs...)
+        Base.show(stream, mime, vec)
+    end
+
     function Base.writemime(stream::IO, mime::MIME"text/markdown", result::QueryResult; kwargs...)
         Base.show(stream, mime, result)
     end
--- test/jupyter.jl
+++ test/jupyter.jl
@@ -6,7 +6,8 @@ else
 end
 
 import Swifter: QueryResult, ResultInfo, App
-import Swifter: path_of_image_from_param, image_scale, save_image_url_to_file
+import Swifter: path_of_image_from_param, save_image_url_to_file
+import Swifter: look_like_view, image_scale
 
 result = QueryResult(ResultInfo(:view,""),App(""),"/query",Dict("lhs"=>[]))
 @test true == mimewritable(MIME"text/markdown", result)
@@ -17,7 +18,21 @@ show(buf, MIME"text/markdown"(), result)
 (path,simple) = path_of_image_from_param(Dict("lhs"=>Any[(:symbol,:vc),(:symbol,:view)]))
 @test path == "%5B%5B%22symbol%22%2C%22vc%22%5D%2C%5B%22symbol%22%2C%22view%22%5D%5D"
 @test simple == "vc.view"
-@test "" == image_scale(result)
 
 ENV["SWIFTER_SAVE_IMAGE"] = false
 @test "" == save_image_url_to_file("", "", "")
+
+str = "<UILabel: 0x7fe29d447580; frame = (10 100; 500 200); "
+(isview, address, xywh) = look_like_view(str)
+@test (true, "0x7fe29d447580", [10,100,500,200]) == (isview, address, xywh)
+@test "width:250.0px; height: 100.0px;" == image_scale(str)
+
+str = "<UILabel: 0x7fe29d447580; frame = {{10, 100}, {500, 200}}; "
+(isview, address, xywh) = look_like_view(str)
+@test (true, "0x7fe29d447580", [10,100,500,200]) == (isview, address, xywh)
+@test "width:250.0px; height: 100.0px;" == image_scale(str)
+
+str = "string"
+(isview, address, xywh) = look_like_view(str)
+@test (false, nothing, nothing)== (isview, address, xywh)
+@test "" == image_scale(str)
